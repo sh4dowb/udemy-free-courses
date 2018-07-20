aed03e7b527226b445f267bbcad9868ab4e5f04c
sh4dowb <cagriytbe@gmail.com>
2018-07-20 20:12:42 +0300

    Added autobuy for courses with coupon
    You may get "maximum requests exceeded" error

--- udemy-autobuy.py
+++ udemy-autobuy.py
@@ -2,65 +2,41 @@ import requests, threading, time, traceback
 from multiprocessing import Queue
 q = Queue()
 
-bearer = ".." # get this from chrome devtools -> network tab
-cookies = """...""" # same as bearer
+cookies = """..."""
+csrftoken = "" # can be found in cookies
 def buy(queue):
 	global bearer
 	global cookies
+	i = 1
+	checkoutdata = '{"shopping_cart":{"items":['
 	while not queue.empty():
 		data = queue.get().split('|')
 		if data[1] != "":
-			#Temporarily disabled, because Udemy will request captcha if this is requested too frequently.
-			#Adding to cart, then mass buying it won't work also, because Udemy requires you to visit course page first for CSRF thing. (https://www.udemy.com/courseidhere)
-			continue
-			courseid = data[0]
-			coupon = data[1]
-			print(courseid)
-			s = requests.session()
-			headers = {
-				'authorization': 'Bearer '+bearer,
-				'accept-encoding': 'gzip, deflate, br',
-				'accept-language': 'en-US,en;q=0.9',
-				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
-				'x-udemy-authorization': 'Bearer '+bearer,
-				'accept': 'application/json, text/plain, */*',
-				'authority': 'www.udemy.com',
-				'x-requested-with': 'XMLHttpRequest',
-			}
-			try:
-				print(s.get('https://www.udemy.com/api-2.0/course-landing-components/'+courseid+'/me/?couponCode='+coupon+'&components=redeem_coupon', headers=headers).text)
-			except:
-				traceback.print_exc()
-				exit()
-			
-			#headers = {
-			#	'accept': 'application/json, text/plain, */*',
-			#	'accept-encoding': 'gzip, deflate, br',
-			#	'accept-language': 'en-US,en;q=0.9',
-			#	'authority': 'www.udemy.com',
-			#	'authorization': 'Bearer *****',
-			#	'cache-control': 'max-age=0',
-			#	'content-type': 'application/json;charset=UTF-8',
-			#	'origin': 'https://www.udemy.com',
-			#	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
-			#	'x-requested-with': 'XMLHttpRequest',
-			#	'x-udemy-authorization': 'Bearer ***',
-			#}
+			if i % 11 == 0:
+				headers = {
+					'Accept': 'application/json, text/plain, */*',
+					'Referer': 'https://www.udemy.com/cart/checkout/',
+					'Origin': 'https://www.udemy.com',
+					'X-Requested-With': 'XMLHttpRequest',
+					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+					'X-CSRFToken': csrftoken,
+					'Content-Type': 'application/json;charset=UTF-8',
+					'cookie': cookies
+				}
 
-			#data = '{"buyables":[{"buyable_object_type":"course","id":'+courseid+',"buyable_context":{}}]}'
-
-			#print(s.post('https://www.udemy.com/api-2.0/shopping-carts/me/cart/', headers=headers, data=data).text)
-			data = '{"shopping_cart":{"items":[{"discountInfo":{"code":"'+coupon+'"},"buyableType":"course","buyableId":'+courseid+',"purchasePrice":{"price_string":"Free","currency_symbol":"\\u20ba","currency":"TRY","amount":0}}]}}'
-			print(requests.post('https://www.udemy.com/payment/shopping-cart-submit/', headers=headers, data=data).text)
+				print(requests.post('https://www.udemy.com/payment/shopping-cart-submit/', headers=headers, data=checkoutdata[:-1]+']}}').text)
+			else:
+				checkoutdata += '{"discountInfo":{"code":"'+data[1]+'"},"purchasePrice":{"amount":0,"currency":"TRY","price_string":"Free","currency_symbol":"\\u20ba"},"buyableType":"course","buyableId":'+data[0]+',"buyableContext":{}},'
+			i += 1
 		else:
+			continue
 			headers = {
-				'authority': 'www.udemy.com',
-				'upgrade-insecure-requests': '1',
-				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
-				'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-				'referer': 'https://www.udemy.com/katalon-studio-step-by-step-for-beginners/',
-				'accept-encoding': 'gzip, deflate, br',
-				'accept-language': 'en-US,en;q=0.9',
+				'Accept': 'application/json, text/plain, */*',
+				'Referer': 'https://www.udemy.com/cart/checkout/',
+				'Origin': 'https://www.udemy.com',
+				'X-Requested-With': 'XMLHttpRequest',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+				'Content-Type': 'application/json;charset=UTF-8',
 				'cookie': cookies
 			}
 			resp = requests.get('https://www.udemy.com/course/subscribe/?courseId='+data[0], headers=headers).text
