cf91b18d85d010ee57291517145bc5b7fc2ee54e
sh4dowb <sh4dowb@protonmail.ch>
2020-06-07 11:31:43 +0000

    Update udemy-autobuy.js

--- udemy-autobuy.js
+++ udemy-autobuy.js
@@ -1,5 +1,5 @@
 // Usage: edit "courses" variable, go to udemy.com, press F12, paste this to console and run.
-// todo: parse rate limiting and wait accordingly, add course and coupons in 1 request
+// todo: parse rate limiting and wait accordingly
 
 
 function getCookie(cname) {
@@ -25,8 +25,44 @@ function sleep(ms) {
 }
 
 async function buyall() {
-  var courses = `3175938|FIBUREWE
-3072680|9139BEF7971B6A165886`;
+  var courses = `3103916|DB0E783E9877F776F2BF
+3173120|A65D1F313B9175C9A95F
+3082522|ANNIVERSARYGIFT6
+2918718|ANNIVERSARYGIFT10
+3157052|ANNIVERSARYGIFT3
+3156620|ANNIVERSARYGIFT2
+3156426|ANNIVERSARYGIFT1
+1386976|FREEJUNERF
+2749466|3D96683DCFE648BD0F49
+2702362|AE6541ADE6F4C270ED5A
+543600|JUN2020FREE2
+3030200|E76F3A3FB5D1A973B805
+2605732|JUNEFIR2020
+2579140|FREEBIE
+1355712|00CF10F7EF2EB504CCDE
+2405932|PLEASEENJOY108
+776356|25C28ADF874BB5D5AD00
+939930|060620_FREE
+2619306|JUNFRE
+3176900|DATA124
+1832998|JUNEFREE
+3115954|F66A302FB
+1508164|MONGOPRACTICAL
+1417860|SHARETHELOVE2020
+2500506|32E514AC6C6C5E87F81C
+2964096|WRITING90
+3016358|CREATE-YOU-KEYLOGGER
+3014480|DISCUDEMY.COM
+3014478|DISCUDEMY.COM
+3016366|ETHICALH1
+2557320|DECORATORS-FREEBIES
+3199916|05AEB4458E99DC26A65E
+2497156|5517E7290B4227395E79
+1971936|A115803A5A1155B0F5D1
+2503534|JUNEFIR2020
+927360|SHARETHELOVE2020
+2120874|JUNEFIR2020
+2996272|EXPERTEXCEL`;
 //EDIT THIS
 
   courses = courses.split("\n");
@@ -34,89 +70,28 @@ async function buyall() {
   for (courseindex in courses) {
     var cid = courses[courseindex].split("|")[0];
     var coupon = courses[courseindex].split("|")[1];
-    const cartResponse = await fetch("https://www.udemy.com/api-2.0/shopping-carts/me/", {
-      "credentials" : "include",
-      "headers" : {
-        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0",
-        "Accept" : "application/json, text/plain, */*",
-        "Accept-Language" : "en-US,en;q=0.5",
-        "Content-Type" : "application/json;charset=utf-8",
-        "X-Requested-With" : "XMLHttpRequest",
-        "Authorization" : "Bearer " + getCookie("access_token"),
-        "X-Udemy-Authorization" : "Bearer " + getCookie("access_token"),
-        "Pragma" : "no-cache",
-        "Cache-Control" : "no-cache"
-      },
-      "referrer" : "https://www.udemy.com/",
-      "mode" : "cors"
-    });
-    var etag = cartResponse.headers.get("etag");
-    await fetch("https://www.udemy.com/api-2.0/shopping-carts/me/cart/", {
-      "credentials" : "include",
-      "headers" : {
-        "Accept" : "application/json, text/plain, */*",
-        "Accept-Language" : "en-US,en;q=0.5",
-        "Content-Type" : "application/json;charset=utf-8",
-        "X-Requested-With" : "XMLHttpRequest",
-        "Authorization" : "Bearer " + getCookie("access_token"),
-        "X-Udemy-Authorization" : "Bearer " + getCookie("access_token"),
-        "Pragma" : "no-cache",
-        "If-Match" : etag,
-        "Cache-Control" : "no-cache"
-      },
-      "referrer" : "https://www.udemy.com/",
-      "body" : '{"buyables":[{"buyable_object_type":"course","id":' + cid + ',"buyable_context":{}}]}',
-      "method" : "POST",
-      "mode" : "cors"
-    });
-    await sleep(500);
-    const resp = await fetch("https://www.udemy.com/api-2.0/shopping-carts/me/discounts/", {
-      "credentials" : "include",
-      "headers" : {
-        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0",
-        "Accept" : "application/json, text/plain, */*",
-        "Accept-Language" : "en-US,en;q=0.5",
-        "Content-Type" : "application/json;charset=utf-8",
-        "X-Requested-With" : "XMLHttpRequest",
-        "Authorization" : "Bearer " + getCookie("access_token"),
-        "X-Udemy-Authorization" : "Bearer " + getCookie("access_token"),
-        "Pragma" : "no-cache",
-        "If-Match" : etag,
-        "Cache-Control" : "no-cache"
-      },
-      "referrer" : "https://www.udemy.com/",
-      "body" : '{"codes":["' + coupon + '"]}',
-      "method" : "POST",
-      "mode" : "cors"
-    });
-    respjson = await resp.json();
-    await sleep(500);
-    if (respjson.cart.length > 0) {
-      await fetch("https://www.udemy.com/payment/checkout-submit/", {
-        "credentials" : "include",
-        "headers" : {
-          "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0",
-          "Accept" : "application/json, text/plain, */*",
-          "Accept-Language" : "en-US,en;q=0.5",
-          "Content-Type" : "application/json;charset=utf-8",
-          "X-Requested-With" : "XMLHttpRequest",
-          "Authorization" : "Bearer " + getCookie("access_token"),
-          "X-Udemy-Authorization" : "Bearer " + getCookie("access_token"),
-          "x-checkout-version" : "2",
-          "X-CSRFToken" : getCookie("csrftoken"),
-          "Pragma" : "no-cache",
-          "If-Match" : etag,
-          "Cache-Control" : "no-cache"
+    await fetch("https://www.udemy.com/payment/checkout-submit/", {
+        "credentials": "include",
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0",
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Content-Type": "application/json;charset=utf-8",
+            "X-Requested-With": "XMLHttpRequest",
+            "Authorization": "Bearer "+getCookie("access_token"),
+            "X-Udemy-Authorization": "Bearer "+getCookie("access_token"),
+            "x-checkout-version": "2",
+            "X-CSRFToken": getCookie("csrftoken"),
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache"
         },
-        "referrer" : "https://www.udemy.com/cart/checkout/",
-        "body" : '{"checkout_event":"Submit","shopping_cart":{"items":[{"discountInfo":{"code":"' + coupon + '"},"purchasePrice":{"amount":0,"currency":"USD","price_string":"Free","currency_symbol":"$"},"buyableType":"course","buyableId":"' + cid + '","buyableContext":{}}],"is_cart":true},"payment_info":{"payment_vendor":"Free","payment_method":"free-method"},"tax_info":{"is_tax_enabled":true,"tax_rate":"7.700","billing_location":{"country_code":"CH","secondary_location_info":null},"currency_code":"usd","transaction_items":[{"udemy_txn_item_reference":"course-' + 
-        cid + '","tax_amount":"0.00","tax_included_amount":"0","tax_excluded_amount":"0.00"}],"tax_breakdown_type":"tax_inclusive"}}',
-        "method" : "POST",
-        "mode" : "cors"
-      });
-      await sleep(3000);
-    }
-    await sleep(2000);
+        "referrer": "https://www.udemy.com/",
+        "body": "{\"checkout_event\":\"Submit\",\"shopping_cart\":{\"items\":[{\"buyableContext\":{\"contentLocaleId\":null},\"buyableId\":"+cid+",\"buyableType\":\"course\",\"discountInfo\":{\"code\":\""+coupon+"\"},\"purchasePrice\":{\"amount\":0,\"price_string\":\"Free\",\"currency_symbol\":\"$\",\"currency\":\"USD\"}}]},\"payment_info\":{\"payment_vendor\":\"Free\",\"payment_method\":\"free-method\"}}",
+        "method": "POST",
+        "mode": "cors"
+    });
+
+    await sleep(5000);
   }
 }
 buyall();
